118eb8b8c6063b9b0e245df19f994c376f2b573a
bansalshefali144@gmail.com <shakti@excellencetechnologies.in>
2021-02-11 15:40:45 +0530

    task1 complated

--- taskassigned/newtask/models.py
+++ taskassigned/newtask/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+    email_id = models.EmailField(max_length=70, null=True, unique=True)
+    location = models.TextField()
--- taskassigned/newtask/views.py
+++ taskassigned/newtask/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from newtask.models import users
+from newtask.serializers import userSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view()
+def user_info(request):
+    # todo = users.objects.all()
+    # todo = users(first_name = "Aman",last_name = "gusain", age = 22, email_id = "aman@gamil.com",location = "delhi 110084")
+    # todo.save()
+    # return HttpResponse(todo.id)
+
+    todo = users.objects.get(pk=1)
+    d = userSerializer(todo)
+    print(d.data,"15555555555555555")
+    return Response({"data":d.data}) 
